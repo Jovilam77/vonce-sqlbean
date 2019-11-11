628ce82fb3c70810823760455a0c0534be414591
jovilam <imjovi@qq.com>
2019-11-11 18:03:17 +0800

    1：修复用户自定义SqlSessionFactory时无法完成自动配置的问题；

--- src/main/java/cn/vonce/sql/config/MybatisAutoConfig.java
+++ src/main/java/cn/vonce/sql/config/MybatisAutoConfig.java
@@ -2,7 +2,9 @@ package cn.vonce.sql.config;
 
 import cn.vonce.sql.orm.mapper.MybatisSqlBeanMapperInterceptor;
 import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
@@ -19,15 +21,30 @@ import java.util.List;
  */
 public class MybatisAutoConfig extends AutoConfig {
 
+    /**
+     * Spring mvc模式使用，Spring boot为空
+     */
     @Autowired(required = false)
     private SqlSessionFactoryBean sqlSessionFactoryBean;
+
+    @Autowired
+    private BeanFactory beanFactory;
 
     @Bean
     @Conditional(ConditionalOnUseMybatis.class)
     public Interceptor interceptor() throws Exception {
         MybatisSqlBeanMapperInterceptor mybatisMapperInterceptor = new MybatisSqlBeanMapperInterceptor();
+        SqlSessionFactory sqlSessionFactory = null;
         if (sqlSessionFactoryBean != null) {
-            List<Interceptor> interceptorList = sqlSessionFactoryBean.getObject().getConfiguration().getInterceptors();
+            sqlSessionFactory = sqlSessionFactoryBean.getObject();
+        } else {
+            try {
+                sqlSessionFactory = beanFactory.getBean(SqlSessionFactory.class);
+            } catch (Exception e) {
+            }
+        }
+        if (sqlSessionFactory != null) {
+            List<Interceptor> interceptorList = sqlSessionFactory.getConfiguration().getInterceptors();
             if (interceptorList != null) {
                 boolean exist = false;
                 for (Interceptor interceptor : interceptorList) {
@@ -37,7 +54,7 @@ public class MybatisAutoConfig extends AutoConfig {
                     }
                 }
                 if (!exist) {
-                    sqlSessionFactoryBean.getObject().getConfiguration().addInterceptor(mybatisMapperInterceptor);
+                    sqlSessionFactory.getConfiguration().addInterceptor(mybatisMapperInterceptor);
                 }
             }
         }
