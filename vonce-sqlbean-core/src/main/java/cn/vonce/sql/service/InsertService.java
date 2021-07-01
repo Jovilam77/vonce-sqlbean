45c201e67699862de417168bfa6adbe22e78e331
Jovilam77 <54133899+Jovilam77@users.noreply.github.com>
2021-07-01 16:32:20 +0800

    Update InsertService.java
    更新InsertService.java

--- vonce-sqlbean-core/src/main/java/cn/vonce/sql/service/InsertService.java
+++ vonce-sqlbean-core/src/main/java/cn/vonce/sql/service/InsertService.java
@@ -1,10 +1,6 @@
 package cn.vonce.sql.service;
 
-import cn.vonce.sql.bean.Column;
-import cn.vonce.sql.bean.Condition;
 import cn.vonce.sql.bean.Insert;
-import cn.vonce.sql.helper.Wrapper;
-
 import java.util.List;
 
 /**
@@ -43,49 +39,5 @@ public interface InsertService<T> {
      * @return
      */
     int inset(Insert<T> insert);
-
-    /**
-     * 备份表和数据到一张新表(表名_+时间)
-     *
-     * @return 默认创建的表名
-     */
-    String backup();
-
-    /**
-     * 备份表和数据到一张指定名称的新表
-     *
-     * @param targetTableName
-     * @return
-     */
-    void backup(String targetTableName);
-
-    /**
-     * 根据条件备份表和数据到一张指定名称的新表
-     *
-     * @param targetTableName
-     * @param columns
-     * @param wrapper
-     * @return
-     */
-    void backup(String targetTableName, Column[] columns, Wrapper wrapper);
-
-    /**
-     * 根据条件将数据复制插入到同样结构的表中
-     *
-     * @param targetTableName
-     * @param wrapper
-     * @return
-     */
-    int copy(String targetTableName, Wrapper wrapper);
-
-    /**
-     * 根据条件将数据复制插入到指定结构的表中
-     *
-     * @param targetTableName
-     * @param columns
-     * @param wrapper
-     * @return
-     */
-    int copy(String targetTableName, Column[] columns, Wrapper wrapper);
 
 }
