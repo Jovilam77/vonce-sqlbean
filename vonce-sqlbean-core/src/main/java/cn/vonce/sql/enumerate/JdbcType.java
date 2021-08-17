af5a205aec6a072af6bff342ab800b60c2374b19
Jovilam77 <54133899+Jovilam77@users.noreply.github.com>
2021-08-17 16:50:39 +0800

    Update JdbcType.java
    增加NUMBER类型

--- vonce-sqlbean-core/src/main/java/cn/vonce/sql/enumerate/JdbcType.java
+++ vonce-sqlbean-core/src/main/java/cn/vonce/sql/enumerate/JdbcType.java
@@ -9,91 +9,80 @@ package cn.vonce.sql.enumerate;
  */
 public enum JdbcType {
 
-    NOTHING(0, 0, 0),
+    NOTHING(0, 0),
 
-    TINYINT(1, 0, 0),
+    TINYINT(0, 0),
 
-    SMALLINT(2, 0, 0),
+    SMALLINT(0, 0),
 
-    INTEGER(3, 0, 0),
+    INTEGER(0, 0),
 
-    BIGINT(4, 0, 0),
+    BIGINT(0, 0),
 
-    LONG(5, 0, 0),
+    LONG(0, 0),
 
-    FLOAT(6, 0, 0),
+    NUMBER(0,0),
 
-    DOUBLE(7, 0, 0),
+    FLOAT(0, 0),
 
-    DECIMAL(8, 10, 2),
+    DOUBLE(0, 0),
 
-    REAL(9, 10, 2),
+    DECIMAL(10, 2),
 
-    NUMERIC(10, 10, 2),
+    REAL(10, 2),
 
-    MONEY(11, 10, 4),
+    NUMERIC(10, 2),
 
-    SMALLMONEY(12, 10, 4),
+    MONEY(10, 4),
 
-    DATE(13, 0, 0),
+    SMALLMONEY(10, 4),
 
-    TIME(14, 0, 0),
+    DATE(0, 0),
 
-    DATETIME(15, 0, 0),
+    TIME(0, 0),
 
-    DATETIME2(16, 0, 0),
+    DATETIME(0, 0),
 
-    TIMESTAMP(17, 0, 0),
+    DATETIME2(0, 0),
 
-    CHAR(18, 255, 0),
+    TIMESTAMP(0, 0),
 
-    NCHAR(19, 255, 0),
+    CHAR(255, 0),
 
-    VARCHAR(20, 255, 0),
+    NCHAR(255, 0),
 
-    VARCHAR2(21, 255, 0),
+    VARCHAR(255, 0),
 
-    NVARCHAR(22, 255, 0),
+    VARCHAR2(255, 0),
 
-    TINYTEXT(23, 0, 0),
+    NVARCHAR(255, 0),
 
-    TEXT(24, 0, 0),
+    TINYTEXT(0, 0),
 
-    NTEXT(25, 0, 0),
+    TEXT(0, 0),
 
-    LONGTEXT(26, 0, 0),
+    NTEXT(0, 0),
 
-    BIT(27, 0, 0),
+    LONGTEXT(0, 0),
 
-    CLOB(28, 0, 0),
+    BIT(0, 0),
 
-    NCLOB(29, 0, 0),
+    CLOB(0, 0),
 
-    BLOB(30, 0, 0),
+    NCLOB(0, 0),
 
-    NBLOB(31, 0, 0);
+    BLOB(0, 0),
+
+    NBLOB(0, 0);
 
 
-    JdbcType(
-            int code, int length, int decimal) {
-        this.code = code;
+    JdbcType(int length, int decimal) {
         this.length = length;
         this.decimal = decimal;
     }
 
-    private int code;
     private int length;
     private int decimal;
-
-
-    public static JdbcType getType(int code) {
-        for (JdbcType jdbcType : values()) {
-            if (jdbcType.code == code) {
-                return jdbcType;
-            }
-        }
-        return null;
-    }
 
     public static JdbcType getType(String name) {
         for (JdbcType jdbcType : values()) {
@@ -104,26 +93,12 @@ public enum JdbcType {
         return null;
     }
 
-    public static String getTypeName(int code) {
-        JdbcType jdbcType = getType(code);
-        return jdbcType == null ? null : jdbcType.name();
-    }
-
-    public static int getCode(String name) {
-        JdbcType jdbcType = getType(name);
-        return jdbcType == null ? -1 : jdbcType.code;
-    }
-
     public boolean isFloat() {
-        if (this.code == NUMERIC.code || this.code == DECIMAL.code || this.code == FLOAT.code ||
-                this.code == DOUBLE.code || this.code == MONEY.code || this.code == SMALLMONEY.code) {
+        if (this.ordinal() == NUMERIC.ordinal() || this.ordinal() == DECIMAL.ordinal() || this.ordinal() == FLOAT.ordinal() ||
+                this.ordinal() == DOUBLE.ordinal() || this.ordinal() == MONEY.ordinal() || this.ordinal() == SMALLMONEY.ordinal()) {
             return true;
         }
         return false;
-    }
-
-    public int getCode() {
-        return code;
     }
 
     public int getLength() {
