fecc3171a661b28d7139e029cec933c0b1a42a31
Jovi <imjovi@qq.com>
2022-08-15 00:44:38 +0800

    新增h2数据库列字段查询，数据类型待完善；

--- vonce-sqlbean-core/src/main/java/cn/vonce/sql/provider/SqlBeanProvider.java
+++ vonce-sqlbean-core/src/main/java/cn/vonce/sql/provider/SqlBeanProvider.java
@@ -624,6 +624,7 @@ public class SqlBeanProvider {
             case PostgreSQL:
             case DB2:
             case H2:
+                return h2ColumnInfoSql(name);
             case Hsql:
                 return hqlColumnInfoSql(name);
             case Derby:
@@ -702,6 +703,27 @@ public class SqlBeanProvider {
         sql.append("AND tb.TABLENAME = '");
         sql.append(tableName);
         sql.append("') ORDER BY cl.COLUMNNUMBER");
+        return sql.toString();
+    }
+
+    private static String h2ColumnInfoSql(String tableName) {
+        StringBuffer sql = new StringBuffer();
+        sql.append("SELECT cl.ORDINAL_POSITION AS cid, ");
+        sql.append("cl.COLUMN_NAME AS name,");
+        sql.append("cl.DATA_TYPE AS type, ");
+        sql.append("cl.IS_NULLABLE AS notnull, ");
+        sql.append("cl.COLUMN_DEFAULT AS dflt_value, ");
+        sql.append("cl.CHARACTER_MAXIMUM_LENGTH AS length, ");
+        sql.append("cl.NUMERIC_SCALE AS scale, ");
+        sql.append("CASE WHEN kcu.TABLE_NAME = cl.TABLE_NAME AND kcu.POSITION_IN_UNIQUE_CONSTRAINT is null THEN 1 ELSE 0 END AS pk ");
+        sql.append("CASE WHEN kcu.TABLE_NAME = cl.TABLE_NAME AND kcu.POSITION_IN_UNIQUE_CONSTRAINT = 1 THEN 1 ELSE 0 END AS fk ");
+        sql.append("cl.REMARKS AS comm ");
+        sql.append("FROM INFORMATION_SCHEMA.COLUMNS cl ");
+        sql.append("LEFT JOIN INFORMATION_SCHEMA.KEY_COLUMN_USAGE kcu ");
+        sql.append("ON kcu.TABLE_NAME = cl.TABLE_NAME AND kcu.COLUMN_NAME = cl.COLUMN_NAME ");
+        sql.append("WHERE cl.TABLE_NAME = '");
+        sql.append(tableName);
+        sql.append("'");
         return sql.toString();
     }
 
