9959854d949d7700993fbe4a6d38274fe008a6be
Jovi <imjovi@qq.com>
2024-07-16 00:52:13 +0800

    核心功能兼容Android的bug修复

--- vonce-sqlbean-core/src/main/java/cn/vonce/sql/enumerate/JavaMapSqliteType.java
+++ vonce-sqlbean-core/src/main/java/cn/vonce/sql/enumerate/JavaMapSqliteType.java
@@ -14,7 +14,7 @@ public enum JavaMapSqliteType {
     NULL(new Class[]{}),
     INTEGER(new Class[]{boolean.class, Boolean.class, byte.class, Byte.class, short.class, Short.class, int.class, Integer.class, long.class, Long.class}),
     REAL(new Class[]{float.class, Float.class, double.class, Double.class, BigDecimal.class}),
-    TEXT(new Class[]{java.sql.Clob.class, char.class, Character.class, String.class, java.sql.Date.class, java.sql.Time.class, java.sql.Timestamp.class, java.util.Date.class, java.time.LocalDateTime.class, java.time.LocalDate.class, java.time.LocalTime.class}),
+    TEXT(new Class[]{java.sql.Clob.class, char.class, Character.class, String.class, java.sql.Date.class, java.sql.Time.class, java.sql.Timestamp.class, java.util.Date.class/*, java.time.LocalDateTime.class, java.time.LocalDate.class, java.time.LocalTime.class*/}),
     BLOB(new Class[]{java.sql.Blob.class, Object.class});
 
     JavaMapSqliteType(Class<?>[] classes) {
