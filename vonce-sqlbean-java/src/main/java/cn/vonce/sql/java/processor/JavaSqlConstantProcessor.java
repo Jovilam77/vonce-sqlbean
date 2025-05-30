579a9cd6b7bc73d963e2666696462e6a837f4acb
Jovi <imjovi@qq.com>
2025-05-30 23:22:16 +0800

    修复不同系统下的路径问题

--- vonce-sqlbean-java/src/main/java/cn/vonce/sql/java/processor/JavaSqlConstantProcessor.java
+++ vonce-sqlbean-java/src/main/java/cn/vonce/sql/java/processor/JavaSqlConstantProcessor.java
@@ -45,7 +45,10 @@ public class JavaSqlConstantProcessor extends SqlConstantProcessor {
         try {
             String classPath = Objects.requireNonNull(getClass().getClassLoader().getResource("")).getPath();
             if (StringUtil.isNotBlank(classPath)) {
-                classPath = classPath.replaceFirst("^/", ""); // 去掉前导的斜杠（在Windows上）
+                //判断是否为Windows系统
+                if (System.getProperty("os.name").toLowerCase().contains("windows")) {
+                    classPath = classPath.replaceFirst("^/", ""); // 去掉前导的斜杠（在Windows上）
+                }
                 classPath = classPath.replaceAll("%20", " "); // 将URL编码的空格转回正常显示
                 String sourceRoot = classPath.substring(0, classPath.lastIndexOf("/target/classes/")) + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator;
                 String javaFilePath = sourceRoot + ((PackageElement) element.getEnclosingElement()).getQualifiedName().toString().replace(".", File.separator) + File.separator + element.getSimpleName().toString() + ".java";
