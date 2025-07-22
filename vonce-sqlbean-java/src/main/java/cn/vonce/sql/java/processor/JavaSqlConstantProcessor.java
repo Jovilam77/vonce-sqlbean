0c9274329c2e81603cc5810326fc1dd38adfed62
Jovi <imjovi@qq.com>
2025-07-23 00:51:09 +0800

    修复Apt在Kotlin环境中找不到Java类报错的问题

--- vonce-sqlbean-java/src/main/java/cn/vonce/sql/java/processor/JavaSqlConstantProcessor.java
+++ vonce-sqlbean-java/src/main/java/cn/vonce/sql/java/processor/JavaSqlConstantProcessor.java
@@ -13,7 +13,6 @@ import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
 import java.io.File;
 import java.util.List;
-import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -43,20 +42,17 @@ public class JavaSqlConstantProcessor extends SqlConstantProcessor {
     @Override
     public String getTableRemarks(Element element) {
         try {
-            String classPath = Objects.requireNonNull(getClass().getClassLoader().getResource("")).getPath();
+            String classPath = new File("").getAbsolutePath();
             if (StringUtil.isNotBlank(classPath)) {
-                //判断是否为Windows系统
-                if (System.getProperty("os.name").toLowerCase().contains("windows")) {
-                    classPath = classPath.replaceFirst("^/", ""); // 去掉前导的斜杠（在Windows上）
-                }
-                classPath = classPath.replaceAll("%20", " "); // 将URL编码的空格转回正常显示
-                String sourceRoot = classPath.substring(0, classPath.lastIndexOf("/target/classes/")) + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator;
+                String sourceRoot = classPath + File.separator + "src" + File.separator + "main" + File.separator + "java" + File.separator;
                 String javaFilePath = sourceRoot + ((PackageElement) element.getEnclosingElement()).getQualifiedName().toString().replace(".", File.separator) + File.separator + element.getSimpleName().toString() + ".java";
-                JavaParserUtil.Declaration declaration = JavaParserUtil.getFieldDeclarationList(sourceRoot, javaFilePath);
-                TypeDeclaration<?> typeDeclaration = declaration.getTypeDeclaration();
-                fieldDeclarationList = declaration.getFieldDeclarationList();
-                if (typeDeclaration != null && typeDeclaration.getComment().isPresent()) {
-                    return JavaParserUtil.getCommentContent(typeDeclaration.getComment().get().getContent());
+                if (new File(javaFilePath).exists()) {
+                    JavaParserUtil.Declaration declaration = JavaParserUtil.getFieldDeclarationList(sourceRoot, javaFilePath);
+                    TypeDeclaration<?> typeDeclaration = declaration.getTypeDeclaration();
+                    fieldDeclarationList = declaration.getFieldDeclarationList();
+                    if (typeDeclaration != null && typeDeclaration.getComment().isPresent()) {
+                        return JavaParserUtil.getCommentContent(typeDeclaration.getComment().get().getContent());
+                    }
                 }
             }
         } catch (Exception e) {
